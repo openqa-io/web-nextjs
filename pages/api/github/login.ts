6cd4ecb928b43eae6a4467cc507b45210219f2e3
broye <fqye@mashixiong.com>
2019-08-29 15:56:12 +0800

    added cors

--- pages/api/github/login.ts
+++ pages/api/github/login.ts
@@ -3,8 +3,18 @@ import {
   NextApiResponse
 } from 'next-server/dist/lib/utils'
 
-export default (_: NextApiRequest, res: NextApiResponse) => {
+import Cors from 'micro-cors'
+
+const cors = Cors({
+  allowMethods: ['GET', 'HEAD', 'POST'],
+})
+
+const server = (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.query)
+  console.log(req.method)
   res.setHeader('Content-Type', 'application/json')
   res.statusCode = 200
   res.end(JSON.stringify({ status: 'ok' }))
 }
+
+export default cors(server)
