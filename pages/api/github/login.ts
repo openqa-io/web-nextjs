9f80106ecf0bae134f4418fe5564841c60e0cc44
broye <fqye@mashixiong.com>
2019-08-30 09:05:04 +0800

    added fetching token

--- pages/api/github/login.ts
+++ pages/api/github/login.ts
@@ -4,14 +4,37 @@ import {
 } from 'next-server/dist/lib/utils'
 
 import Cors from 'micro-cors'
+import fetch from 'isomorphic-unfetch'
+
+import {
+  githubGetTokenUrl,
+  githubClientId
+} from '../../../lib/constants'
 
 const cors = Cors({
   allowMethods: ['GET', 'HEAD', 'POST'],
 })
 
-const server = (req: NextApiRequest, res: NextApiResponse) => {
+const server = async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.query)
   console.log(req.method)
+  const { code } = req.query
+
+  const tokens = await fetch(githubGetTokenUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      client_id: githubClientId,
+      client_secret: process.env.github_secret,
+      code
+    })
+  })
+
+  console.log(tokens.json())
+
   res.setHeader('Content-Type', 'application/json')
   res.statusCode = 200
   res.end(JSON.stringify({ status: 'ok' }))
