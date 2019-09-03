0699a3b0649b2ef64e88e45bdca3f93fc374fb04
broye <fqye@mashixiong.com>
2019-09-03 10:55:51 +0800

    added verify login and load user info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,14 @@ import createStore from '../lib/store/root.store'
 import { Provider } from "react-redux"
 import { Store } from 'redux'
 
+import { CookieNames } from '../lib/constants'
 import { isServer } from '../lib/helpers/dom'
 import App, { AppContext } from 'next/app'
 
 import { NextPageContext } from 'next'
 import cookies from 'next-cookies'
+
+import * as actions from '../lib/actions/root.actions'
 
 interface Props extends AppContext {
   ctx: NextPageContext & { store: Store }
@@ -21,15 +24,40 @@ class MyApp extends App<any, any> {
   static async getInitialProps({ Component, ctx }: Props) {
 
     // we can dispatch from here too
-    const oqaCookies = cookies(ctx)
-    console.log('cookies are: ', oqaCookies)
-    /* if (isServer) {
-     *   import('../lib/db').then(({ getGithubUserInfo }: any) => {
-     *     ctx.store.dispatch({ type: 'FOO', payload: 'foo' })
-     *   })
 
-     * }
-     */
+    if (isServer()) { // isServer, validate login
+      try {
+        const cookiesFromReq = cookies(ctx)
+        if (cookiesFromReq) {
+          const loginInfo = JSON.parse(cookiesFromReq[CookieNames.OpenQA])
+          const { uid, token } = loginInfo
+
+          if (uid && token) {
+            const { getGithubUserInfo } = await import('../lib/db') as any
+            const {
+              github_uid,
+              github_user_avatar
+            } = getGithubUserInfo(uid, token)
+
+            if (github_uid) {
+              ctx.store.dispatch(actions.setUserInfo({
+                uid,
+                token,
+                githubName: github_uid,
+                githubAvatar: github_user_avatar
+              }))
+            } else
+              ctx.store.dispatch(actions.setUserInfo({
+              }))
+          } else
+            ctx.store.dispatch(actions.setUserInfo({
+            }))
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
 
     return { pageProps }
